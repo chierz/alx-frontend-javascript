96094a66ee876a0b59fd21663234ad7d142a7339
chierz <bonanwaeze@gmail.com>
2025-01-20 05:41:29 +0000

    data manipation in js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default function updateStudentGradeByCity(students, city, newGrade){
+  return students
+  //.filter((student) => student.location.trim().toLowerCase() === city.trim().toLowerCase())
+    .filter((student) => student.location == city)
+    .map((student) => {
+      let grades = newGrade.find((grade) => grade.studentId == student.id);
+      let grade = grades ? grades.grade : "N/A";
+      return { ...student, grade };
+     });
+}
